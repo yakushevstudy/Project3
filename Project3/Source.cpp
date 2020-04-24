325fda12a1705f6fc8698d52744a0f87115976df
yakushevstudy <yakushevstudy@gmail.com>
2020-04-24 16:40:51 +0300

    Добавьте файлы проекта.

---
+++ Project3/Source.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <math.h>
+#include <cstdio>
+using namespace std;
+int main()
+{
+	cout << "Hello world" << endl;
+	setlocale(LC_ALL, "ru");
+	int choice;
+	float a, b;
+	cout << "Enter 2 numbers";
+	cin >> a >> b;
+
+	cout << "Choose operation" << endl;
+	cout << "1) Sum" << endl
+		<< "2) Subtraction" << endl
+		<< "3) Product" << endl
+		<< "4) Difference" << endl
+		<< "5) Square root" << endl
+		<< "6) Power" << endl;
+	cin >> choice;
+	switch (choice)
+	{
+	case 1:
+		cout << "Sum=" << a + b << endl;
+		break;
+	case 2:
+		cout << "Subtraction=" << a - b << endl;
+		break;
+	case 3:
+		cout << "Product=" << a * b << endl;
+		break;
+	case 4:
+		if (b < 0)cout << "Difference=" << a / b << endl;
+		else (b == 0);
+		cout << "Ïîðà â äóðêó" << endl;
+		break;
+	case 5:
+		cout << "Square root=" << (sqrt(a)) << endl;
+		break;
+	case 6:
+		cout << "Power=" << (pow(a, b)) << endl;
+		break;
+	default:
+		cout << "Error" << endl;
+		break;
+
+
+
+
+
+	}
+
+
+
+
+}
